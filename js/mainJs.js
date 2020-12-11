a3bb3e42465731eaf6184bfd109582fdaf8ee79a
Ahmed Mrad <mrad.ahmed5@gmail.com>
2020-12-11 15:35:46 +0100

    front end updates

---
+++ js/mainJs.js
@@ -0,0 +1,18 @@
+var turn= ;
+var boxes = ;
+var selector = ;
+
+
+// select marker
+
+
+// after marker selection trigger fading to the game
+
+
+// select marker on board
+
+
+//add win method
+
+
+//replay ==> reset the game from the beginning
